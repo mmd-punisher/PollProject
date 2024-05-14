b941741aad9d4b17f359cc2a870441c6d88fe78d
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-05-14 14:05:27 +0330

    Admin Customization

--- cmdq/admin.py
+++ cmdq/admin.py
@@ -1,26 +1,44 @@
 from django.contrib import admin
 from .models import Question, Choice, Vote, User
 
-admin.site.site_header = 'CMDQ Administration'
-admin.site.site_title = 'Voting Admin Area'
-admin.site.index_title = 'Welcome to Voting Admin Area'
+# admin.site.site_header = 'CMDQ Administration'
+admin.site.site_header = 'پنل مدیریت CMDQ'
+admin.site.site_title = 'پنل مدیریت نظرسنجی ادمین'
+admin.site.index_title = 'به پنل مدیریت نظرسنجی ادمین خوش آمدید'
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 5
-    verbose_name = 'Choice'
-    verbose_name_plural = 'Choices'
+    verbose_name = 'انتخاب'
+    verbose_name_plural = 'انتخاب ها'
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['question_text']}),
                  ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date',)
+    ordering = ('id',)
 
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question__id', 'question', 'choice')
+    list_filter = ('user', 'question__id', 'choice', 'question')
+    ordering = ('-user', 'question__id')
+
+    # @staticmethod
+    # def user__id(self, obj):
+    #     return obj.user.id
+
+    def question__id(self, obj):
+        return obj.question.id
+
+
+admin.site.register(Vote, VoteAdmin)
 
 admin.site.register(Question, QuestionAdmin)
 # Vote Admin needed
-admin.site.register(Vote)
+# admin.site.register(Vote)
 # class VoteAdmin(admin.ModelAdmin):
 admin.site.register(User)
--- cmdq/models.py
+++ cmdq/models.py
@@ -25,54 +25,69 @@ class User(models.Model):
                                                                          message='سن بیشتر از 150 سال قابل قبول نیست')])
     job = models.CharField(max_length=255, null=True, verbose_name='شغل')
     work_experience = models.PositiveSmallIntegerField(default=0, null=True, verbose_name='سابقه شغلی',
-                                                       validators=[MaxValueValidator(60,
-                                                                                     message='حداکثر سابقه شغلی 60 سال است')])
+                                                       validators=[MaxValueValidator(
+                                                           60,
+                                                           message='حداکثر سابقه شغلی 60 سال است')])
     education = models.CharField(max_length=2, choices=EDUCATION_Choices, null=True, verbose_name="تحصیلات")
     marital_status = models.CharField(max_length=2, choices=MARITAL_Choices, null=True, verbose_name="وضعیت تعهد")
     height = models.PositiveSmallIntegerField(verbose_name='قد (به سانتی متر)',
                                               validators=[MinValueValidator(110, message='حداقل قد 110 سانتی متر است'),
-                                                          MaxValueValidator(250,
-                                                                            message='قد وارد شده بیشتر از مقدار معقول است')])  # cm
+                                                          MaxValueValidator(
+                                                              250,
+                                                              message='قد وارد شده بیشتر از مقدار معقول است')])  # cm
     weight = models.FloatField(verbose_name='وزن (به متر)',
                                validators=[MinValueValidator(20, message='وزن مورد نظر کمتر از مقدار معقول است'),
                                            MaxValueValidator(250, message='وزن وارد شده بیشتر از مقدار محدود شده است')])
     bmi = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+        ordering = ['first_name', 'last_name']
 
     def save(self, *args, **kwargs):
         self.bmi = round(self.weight / ((self.height / 100) ** 2), 2)
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}, {self.id}"
-
-    class Meta:
-        ordering = ['first_name', 'last_name']
+        return f"{self.first_name} {self.last_name}"
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=350)
     pub_date = models.DateTimeField('date published', default=datetime.now)
 
+    class Meta:
+        verbose_name = 'سوال'
+        verbose_name_plural = 'سوالات'
+
     def __str__(self):
         return self.question_text
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=150)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='سوال')
+    choice_text = models.CharField(max_length=150, verbose_name='متن گزینه ها')
 
     # votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'انتخاب'
+        verbose_name_plural = 'انتخاب ها'
 
     def __str__(self):
         return self.choice_text
 
 
 class Vote(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='سوال')
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, verbose_name='انتخاب')
 
     # choice_box = models.CharField(max_length=3, null=True, blank=True)
+    class Meta:
+        verbose_name = 'رای'
+        verbose_name_plural = 'رای ها'
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - {self.question.question_text} - {self.choice.choice_text}"
